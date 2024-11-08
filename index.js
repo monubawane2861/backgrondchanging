b2278e85bed1b7dd2975d07f39ad19ff31f9db50
monubawane2861 <monubawane2861>
2024-11-08 23:36:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+//
+
+
+let pageColorE1 = document.getElementById("pageColor");
+
+let bgGrayE1 = document.getElementById("bgGray");
+
+let bgGreenE1 = document.getElementById("bgGreen");
+
+let bgBlueE1 = document.getElementById("bgBlue");
+
+let  bgPurpleE1 = document.getElementById("bgPurple");
+
+let  colorChangeE1 = document.getElementById("colorChange");
+
+function checkBgGray(){
+
+  pageColorE1.style.backgroundColor ="#e0e0e0";
+
+  colorChangeE1.textContent ="#e0e0e0"
+
+}
+
+
+function checkBgGreen(){
+
+  pageColorE1.style.backgroundColor ="#6fcf97";
+
+  colorChangeE1.textContent ="#6fcf97"
+
+  
+}
+
+
+
+function checkBgBlue(){
+
+  pageColorE1.style.backgroundColor ="#56ccf2";
+
+  colorChangeE1.textContent ="#56ccf2"
+
+}
+
+
+function checkBgPurple(){
+
+  pageColorE1.style.backgroundColor ="#bb6bd9";
+
+  colorChangeE1.textContent ="#bb6bd9"
+
+
+}
